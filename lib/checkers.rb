1c6708aa05672fc0432579341b63eeaadcdaeba5
Danny Bosan <danbosan@servicesidekick.com>
2011-06-30 15:33:53 -0700

    add loader

---
+++ lib/checkers.rb
@@ -0,0 +1,4 @@
+require File.expand_path('../../lib/checkers/piece', __FILE__)
+require File.expand_path('../../lib/checkers/player', __FILE__)
+require File.expand_path('../../lib/checkers/board', __FILE__)
+require File.expand_path('../../lib/checkers/game', __FILE__)
