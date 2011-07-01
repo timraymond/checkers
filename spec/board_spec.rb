2f725995b8e783e230248e12d56e705744578d20
Danny Bosan <danbosan@servicesidekick.com>
2011-06-30 20:16:21 -0500

    define more board specs

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,4 +5,28 @@ describe Checkers::Board do
     board = Checkers::Board.new
     board.cells.count.should == 64
   end
+
+  it "should have the first cell as dark" do
+    board = Checkers::Board.new
+    board.cells["c1:r1"][:color].should == :dark
+  end
+
+  it "should have the last cell as dark" do
+    board = Checkers::Board.new
+    board.cells["c8:r8"][:color].should == :dark
+  end
+
+  it "should have 8 rows" do
+    board = Checkers::Board.new
+    (1..8).each do |i|
+      board.cells["c1:r#{i}"].should.class == Hash
+    end
+  end
+
+  it "should have 8 columns" do
+    board = Checkers::Board.new
+    (1..8).each do |i|
+      board.cells["c#{i}:r1"].should.class == Hash
+    end
+  end
 end
