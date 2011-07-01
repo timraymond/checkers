8731e2641af384306286707b42a973374fdf517a
Danny Bosan <danbosan@servicesidekick.com>
2011-06-30 20:17:09 -0500

    initial player class

---
+++ lib/checkers/player.rb
@@ -0,0 +1,11 @@
+module Checkers
+  class Player
+    attr_reader :color, :pieces
+    attr_accessor :captured_pieces
+
+    def initialize(color)
+      @color = color
+      @pieces = (1..12).inject([]) {|array| array << Piece.new(color)}
+    end
+  end
+end
